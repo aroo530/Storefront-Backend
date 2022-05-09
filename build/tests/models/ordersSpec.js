206813121ad2b11d020e42526f6ec73c7e587663
Arsany Milad <arsanymilad45@gmail.com>
2022-05-09 15:24:50 +0200

    minors

--- build/tests/models/ordersSpec.js
+++ build/tests/models/ordersSpec.js
@@ -2,43 +2,43 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const orders_1 = require("../../models/orders");
 const users_1 = require("../../models/users");
-describe("OrderOperations", () => {
+describe('OrderOperations', () => {
     const operations = new orders_1.OrderOperations();
     const userOperations = new users_1.UserOperations();
     const tempUser = {
-        first_name: "Ahmed",
-        last_name: "mostafa",
-        password: "ORDERoperations",
+        first_name: 'Ahmed',
+        last_name: 'mostafa',
+        password: 'ORDERoperations',
     };
     userOperations.createUser(tempUser);
     const tempOrder = {
-        user_id: "2",
-        status: "active",
+        user_id: '2',
+        status: 'active',
     };
     const updateOrder = {
-        user_id: "2",
-        status: "completed",
+        user_id: '2',
+        status: 'completed',
     };
-    it("should create a new Order", async () => {
+    it('should create a new Order', async () => {
         const newOrder = await operations.createOrder(tempOrder);
         expect(newOrder.user_id).toBe(tempOrder.user_id);
         expect(newOrder.status).toBe(tempOrder.status);
     });
-    it("should get Order", async () => {
-        const Order = await operations.getOrder("2");
-        expect(Order.status).toEqual("active");
+    it('should get Order', async () => {
+        const Order = await operations.getOrder('2');
+        expect(Order.status).toEqual('active');
     });
-    it("should get all Orders", async () => {
+    it('should get all Orders', async () => {
         expect((await operations.getAllOrders()).length).toEqual(1);
     });
-    it("should update Order", async () => {
+    it('should update Order', async () => {
         const order = await operations.updateOrderStatus(updateOrder);
-        expect(order.status).toEqual("completed");
+        expect(order.status).toEqual('completed');
     });
-    it("should delete Order", async () => {
-        const order = await operations.deleteOrder("2");
+    it('should delete Order', async () => {
+        const order = await operations.deleteOrder('2');
         expect(order.user_id).toEqual(updateOrder.user_id);
         expect(order.status).toEqual(updateOrder.status);
-        userOperations.deleteUser("Ahmed");
+        userOperations.deleteUser('Ahmed');
     });
 });
