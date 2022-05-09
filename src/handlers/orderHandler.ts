d32e0a12e2c683c201c619da737d89add77c02c1
Arsany Milad <arsanymilad45@gmail.com>
2022-05-09 09:41:10 +0200

    Update orderHandler.ts

--- src/handlers/orderHandler.ts
+++ src/handlers/orderHandler.ts
@@ -9,19 +9,28 @@ const operations: OrderOperations = new OrderOperations();
 const userOperations: UserOperations = new UserOperations();
 const productOperations = new ProductOperations();
 const getOrder = async (req: Request, res: Response): Promise<void> => {
-    const user: User = await userOperations.getIdByFirstName(
-        req.params.user_id
-    );
-    const order: Order = await operations.getOrder(user.id!);
-    res.json(order);
+    try {
+        const user: User = await userOperations.getIdByFirstName(
+            req.params.user_id
+        );
+        const order: Order = await operations.getOrder(user.id!);
+        res.json(order);
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
 };
 
 const getOrders = async (req: Request, res: Response): Promise<void> => {
-    const orders: Order[] = await operations.getAllOrders();
-    res.json(orders);
+    try {
+        const orders: Order[] = await operations.getAllOrders();
+        res.json(orders);
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
 };
 
 const createOrder = async (req: Request, res: Response): Promise<void> => {
+    try {
     const getId: User = await userOperations.getIdByFirstName(req.body.user_id);
     if (getId) {
         let order: Order = req.body;
@@ -31,26 +40,40 @@ const createOrder = async (req: Request, res: Response): Promise<void> => {
     } else {
         res.status(404).json({ message: 'User not found' });
     }
+    
+} catch (error) {
+    res.status(500).json({ message: error });
+}
 };
 
 const updateOrderStatus = async (
     req: Request,
     res: Response
 ): Promise<void> => {
-    const getId: User = await userOperations.getIdByFirstName(req.body.user_id);
-    let order: Order = req.body;
-    order.user_id = getId.id!;
+    try {
+        const getId: User = await userOperations.getIdByFirstName(
+            req.body.user_id
+        );
+        let order: Order = req.body;
+        order.user_id = getId.id!;
 
-    const updatedOrder: Order = await operations.updateOrderStatus(order!);
-    res.json(updatedOrder);
+        const updatedOrder: Order = await operations.updateOrderStatus(order!);
+        res.json(updatedOrder);
+    } catch (error) {
+        res.status(404).json({ message: 'not found' });
+    }
 };
 
 const deleteOrder = async (req: Request, res: Response): Promise<void> => {
-    const getId: User = await userOperations.getIdByFirstName(
-        req.params.user_id
-    );
-    await operations.deleteOrder(getId.id!);
-    res.json('Order deleted');
+    try {
+        const getId: User = await userOperations.getIdByFirstName(
+            req.params.user_id
+        );
+        await operations.deleteOrder(getId.id!);
+        res.json('Order deleted');
+    } catch (error) {
+        res.status(404).json({ message: 'not found' });
+    }
 };
 
 const addProductToOrder = async (
@@ -58,38 +81,48 @@ const addProductToOrder = async (
     res: Response
 ): Promise<void> => {
     //we will get the order id from the user id
-    const user: User = await userOperations.getIdByFirstName(
-        req.params.user_id
-    );
-    const order: Order = await operations.getOrder(user.id!);
+    try {
+        const user: User = await userOperations.getIdByFirstName(
+            req.params.user_id
+        );
+        const order: Order = await operations.getOrder(user.id!);
 
-    const product: Product = await productOperations.getProduct(
-        req.body.product_name
-    );
-    const cart: Cart = await operations.addProductToOrder(
-        order.id!,
-        product.id!,
-        req.body.quantity
-    );
-    res.json(cart);
+        const product: Product = await productOperations.getProduct(
+            req.body.product_name
+        );
+        const cart: Cart = await operations.addProductToOrder(
+            order.id!,
+            product.id!,
+            req.body.quantity
+        );
+        res.json(cart);
+    } catch (error) {
+        res.status(404).json({ message: 'not found' });
+    }
 };
 const checkStatus = async (
     req: Request,
     res: Response,
     next: express.NextFunction
 ): Promise<void> => {
-    const user: User = await userOperations.getIdByFirstName(req.body.user_id);
-    const order: Order = await operations.getOrder(user.id!);
-    if (order.status === 'active') {
-        next();
-    } else {
-        res.status(400).send('Complete orders cannot be updated');
+    try {
+        const user: User = await userOperations.getIdByFirstName(
+            req.body.user_id
+        );
+        const order: Order = await operations.getOrder(user.id!);
+        if (order.status === 'active') {
+            next();
+        } else {
+            res.status(400).send('Complete orders cannot be updated');
+        }
+    } catch (error) {
+        res.status(404).send('Order not found');
     }
 };
 
 export const orderOperationsRouts = (app: express.Application): void => {
     app.get('/orders/:user_id', verifyAuthToken, getOrder);
-    app.get('/orders', getOrders);
+    app.get('/orders', verifyAuthToken, getOrders);
     app.post('/orders', verifyAuthToken, createOrder);
     app.post('/orders/:user_id/products', verifyAuthToken, addProductToOrder);
     app.put('/orders', verifyAuthToken, checkStatus, updateOrderStatus);
